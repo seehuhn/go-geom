0d08c2b32ece054d7242c7a5560677114163760e
Jochen Voss <voss@seehuhn.de>
2025-06-12 23:16:19 +0100

    cosmetic fixes

--- path/path.go
+++ path/path.go
@@ -32,30 +32,25 @@ const (
 	CmdClose
 )
 
-// Contour iterates over individual segments.
+// Path iterates over individual segments.
 // The first argument is the segment type, which determines the number of points:
 //   - CmdMoveTo: 1 point (starts a new sub-path at the given point)
 //   - CmdLineTo: 1 point (endpoint of the line segment)
 //   - CmdQuadTo: 2 points (control point and endpoint of the quadratic Bezier curve)
 //   - CmdCubeTo: 3 points (control point 1, control point 2, and endpoint of the cubic Bezier curve)
 //   - CmdClose: 0 points (straight line from the current point to start of the current sub-path)
-type Contour iter.Seq2[Command, []Point]
+type Path iter.Seq2[Command, []Point]
 
-// Compound represents a path that consists of multiple contours.
-type Compound interface {
-	Contours() iter.Seq[Contour]
-}
-
-// ToCubic converts all quadratic segments in the contour to cubic segments.
+// ToCubic converts all quadratic segments in the path to cubic segments.
 // The resulting representation describes exactly the same path,
 // but is less efficient (since two control points are used instead of one).
-func ToCubic(c Contour) Contour {
+func ToCubic(p Path) Path {
 	// https://pomax.github.io/bezierinfo/#reordering
 	return func(yield func(Command, []Point) bool) {
 		var current Point
 		var start Point
 		var buf [3]Point
-		for cmd, pts := range c {
+		for cmd, pts := range p {
 			if cmd == CmdQuadTo {
 				buf[0].X = current.X*1/3 + pts[0].X*2/3
 				buf[0].Y = current.Y*1/3 + pts[0].Y*2/3
